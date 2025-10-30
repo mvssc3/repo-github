a58e4edab7ac71988fd955a94956e845539cf678
Marlon Vinícius Sobania <mvssc3@gmail.com>
2025-10-30 16:09:12 -0300

    arquivo teste removido

--- teste.py
+++
@@ -1,9 +0,0 @@
-print("Hello, World!")
-
-### arquivo untracked.txt criado ###
-
-### segunda modificação no teste.py ###
-print("This is a modification.")
-
-### terceira modificação no teste.py ###
-print("This is another modification.")
